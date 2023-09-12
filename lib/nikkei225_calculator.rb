eb3718590b9c7fd70a879ad4ade1b8fad1a41cc3
dogwood008 <dogwood008+github@gmail.com>
2023-09-12 21:55:18 +0900

    :sparkles: Load CSVs

--- lib/nikkei225_calculator.rb
+++ lib/nikkei225_calculator.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
+require 'csv'
+require 'bigdecimal'
+
 require_relative 'nikkei225_calculator/version'
 
 module Nikkei225Calculator
-  class Error < StandardError; end
-  # Your code goes here...
+  class Csv
+    attr_reader :adj_factor, :avg_weight
+
+    def self.converter_proc
+      Proc.new do |field, field_info|
+        next field unless field.include?('.')
+        BigDecimal(field.delete_suffix('%'))
+      end
+    end
+
+    def initialize(adj_factor:, avg_weight:)
+      @adj_factor = CSV.read(adj_factor, headers: true, converters: self.class.converter_proc)
+      @avg_weight = CSV.read(avg_weight, headers: true, converters: self.class.converter_proc)
+    end
+  end
 end
--- spec/nikkei225_calculator_spec.rb
+++ spec/nikkei225_calculator_spec.rb
@@ -8,19 +8,37 @@ RSpec.describe Nikkei225Calculator do
     end
   end
 
-  describe '#initialize' do
-    let(:adj_factor_csv_filename) { 'nikkei_225_price_adjustment_factor_jp.csv' }
-    let(:adj_factor) { RSpec.root.join(adj_factor_csv_filename) }
-    let(:avg_weight_csv_filename) { 'nikkei_225_average_weight_jp.csv' }
-    let(:avg_weight) { RSpec.root.join(avg_weight_csv_filename) }
+  describe Nikkei225Calculator::Csv do
+    describe '#initialize' do
+      let(:adj_factor_csv_filename) { 'nikkei_225_price_adjustment_factor_jp.csv' }
+      let(:adj_factor) { RSpec.root.join(adj_factor_csv_filename) }
+      let(:avg_weight_csv_filename) { 'nikkei_stock_average_weight_jp.csv' }
+      let(:avg_weight) { RSpec.root.join(avg_weight_csv_filename) }
 
-    subject { Nikkei225Calculator.new(adj_factor:, avg_weight:) }
+      let(:csv_instance) { described_class.new(adj_factor:, avg_weight:) }
 
-    it 'loads nikkei_225_price_adjustment_factor_jp.csv successfully' do
-      expect(subject.avg_weight.headers).to eq(%w[対象日付 コード 銘柄名 株価換算係数 業種 セクター])
-    end
-    it 'loads nikkei_225_average_weight_jp.csv successfully' do
-      expect(subject.avg_weight.headers).to eq(%w[日付 コード 社名 業種 セクター ウエート])
+      describe 'loading nikkei_225_price_adjustment_factor_jp.csv successfully' do
+        subject { csv_instance.adj_factor }
+        it 'load successfully' do
+          expect(subject.headers).to eq(%w[対象日付 コード 銘柄名 株価換算係数 業種 セクター])
+        end
+        it do
+          expect(subject.first.to_h.values).to eq(
+            ["2023/09/12", "1234", "テスト株式会社", BigDecimal('1.0'), "水産", "消費"]
+          )
+        end
+      end
+      describe 'loading nikkei_stock_average_weight_jp.csv successfully' do
+        subject { csv_instance.avg_weight }
+        it do
+          expect(subject.headers).to eq(%w[日付 コード 社名 業種 セクター ウエート])
+        end
+        it do
+          expect(subject.first.to_h.values).to eq(
+            ["2023/09/12", "1234", "テスト株式会社", "水産", "消費", BigDecimal('0.1234')]
+          )
+        end
+      end
     end
   end
 end
